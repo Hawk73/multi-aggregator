8ba8609ffa37c6bd7ca10ee3e1c74f2350378f56
hawk73 <hawk.bms@gmail.com>
2017-08-16 01:52:48 +0400

    Some changes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
 include FactoryGirl::Syntax::Methods
 
 user = create(:user, email: 'test@test.com', password: 'password')
+
 create_list(:provider, 2, user: user)
+
+create_list(:request, 2, user: user)
